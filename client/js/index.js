868003a671c41b829a999efde09263e17e275aba
rickyall <783814127@qq.com>
2016-05-03 14:56:17 +0800

    实现了排序功能

--- client/js/index.js
+++ client/js/index.js
@@ -1,29 +1,50 @@
 $.get('http://localhost:8888/DAInews',function(data){
-  // alert(data);
   //创建数据集
   data = [{
     "name":'CIA-1602-huqy',
-    "prevalue":'1.1902',
+    "netvalue":'1.1902',
     "dayrate":'+4.23%',
     "monthrate":"7.25%",
     "totalrate":"19.02%",
     "back":"6.74%",
     "xiapu":"8.74%"
+  }, {"name":'CIA-1602-qisc',
+      "netvalue":'2.1902',
+      "dayrate":'+3.23%',
+      "monthrate":"4.25%",
+      "totalrate":"20.02%",
+      "back":"3.74%",
+      "xiapu":"2.74%"
   }, {"name":'CIA-1602-huqy',
-      "prevalue":'1.1902',
-      "dayrate":'+4.23%',
-      "monthrate":"7.25%",
-      "totalrate":"19.02%",
-      "back":"6.74%",
-      "xiapu":"8.74%"
-  }, {"name":'CIA-1602-huqy',
-      "prevalue":'1.1902',
+      "netvalue":'1.1902',
       "dayrate":'+4.23%',
       "monthrate":"7.25%",
       "totalrate":"19.02%",
       "back":"6.74%",
       "xiapu":"8.74%"
 }]
+  //添加HTML内容
+  addHtml(data,"data-list");
+
+  //实现点击效果正序排列
+  $("th").each(function(){
+    var name = $(this).attr("id");//获取th的ID值
+    sortData(data,name);//按照该列进行排序
+  })
+});
+
+
+
+//按照关键字进行排序
+function sortData(data,name){
+  $("#"+name).click(function(){
+    data.sort(getSortFun('desc',name))//对数据进行排序
+    $("#data-list").empty();//将data-list的列表清空
+    addHtml(data,"data-list");
+  })
+}
+//将排序后的数据添加到标签中
+function addHtml(data,id){
   data.forEach(function(d,i){
     var html = "<tr>\
         <td>"+d.name+"</td>\
@@ -34,6 +55,12 @@ $.get('http://localhost:8888/DAInews',function(data){
         <td>"+d.back+"</td>\
         <td>"+d.xiapu+"</td>\
         </tr>"
-    $("#data-list").append(html)
+    $("#"+id).append(html)
   })
-});
+}
+//排序函数
+function getSortFun(order, sortBy) {
+  var ordAlpah = (order == 'asc') ? '>' : '<';
+  var sortFun = new Function('a', 'b', 'return a.' + sortBy + ordAlpah + 'b.' + sortBy + '?1:-1');
+  return sortFun;
+}
