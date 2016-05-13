a35bf17b9353f089359417c163d49875ca0637fd
rickyall <783814127@qq.com>
2016-05-13 09:38:52 +0800

    更新API接口

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
-var DAInews = require('../db/models').DAInews;
+var CombStock = require('../db/models').CombStock;
 var router = express.Router();
 
 /* 获取所有的定增新闻 */
-router.get('/DAInews', function(req, res, next) {
-    DAInews.findAll({limit:5,order:'publish_time desc'}).then(function(news){
+router.get('/comb', function(req, res, next) {
+    CombStock.findAll().then(function(news){
       res.json(news);
     });
 });
