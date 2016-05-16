c1c730acf33e1ef2a79ff03a4793e44105684f05
rickyall <783814127@qq.com>
2016-05-16 15:14:58 +0800

    添加docker

--- server/api/index.js
+++ server/api/index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var CombStock = require('../db/models').CombStock;
 var t = require('../help/time');
 var router = express.Router();
+/*设置根目录*/
+router.get('/',function(req,res){
+  res.render('index');
+})
 
 /* 获取所有的定增新闻 */
 router.get('/comb', function(req, res, next) {
